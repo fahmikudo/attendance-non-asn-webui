8b5fb100a566f687e89b13774ccc3a09c3f4d176
fahmikudo <fahmi.hidayatullah12@gmail.com>
2020-02-18 08:54:06 +0700

    Updates

---
+++ precache-manifest.6c249a0144b5b822ec4a78d1d5864edf.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1628b23194fde977b1ad499626f879ee",
+    "url": "/attendance-non-asn-webui/index.html"
+  },
+  {
+    "revision": "42e8cfcd67a6cc1930e5",
+    "url": "/attendance-non-asn-webui/static/css/2.6513b572.chunk.css"
+  },
+  {
+    "revision": "9d4427cdba9788b4d515",
+    "url": "/attendance-non-asn-webui/static/css/main.0450500e.chunk.css"
+  },
+  {
+    "revision": "42e8cfcd67a6cc1930e5",
+    "url": "/attendance-non-asn-webui/static/js/2.d3480bf3.chunk.js"
+  },
+  {
+    "revision": "aaf1612b979a76bafb26e88463280f4b",
+    "url": "/attendance-non-asn-webui/static/js/2.d3480bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4427cdba9788b4d515",
+    "url": "/attendance-non-asn-webui/static/js/main.855d98fe.chunk.js"
+  },
+  {
+    "revision": "b4a311fddc5998cbfbf7",
+    "url": "/attendance-non-asn-webui/static/js/runtime-main.ce19da5c.js"
+  },
+  {
+    "revision": "fb98ed1700e8dfaf0764c11fc36a0a05",
+    "url": "/attendance-non-asn-webui/static/media/NunitoSans-Regular.fb98ed17.ttf"
+  },
+  {
+    "revision": "f46f486edefeba66ac4b538dd593d656",
+    "url": "/attendance-non-asn-webui/static/media/Quicksand-Regular.f46f486e.ttf"
+  },
+  {
+    "revision": "b5d4adffb6b244532a12c78f4124af70",
+    "url": "/attendance-non-asn-webui/static/media/Ubuntu-Bold.b5d4adff.ttf"
+  },
+  {
+    "revision": "f5a896693ead0b135e7e3f7f695463b1",
+    "url": "/attendance-non-asn-webui/static/media/Ubuntu-Light.f5a89669.ttf"
+  },
+  {
+    "revision": "6bb9beb4ae22b811bfd5369d517e2f7c",
+    "url": "/attendance-non-asn-webui/static/media/Ubuntu-Regular.6bb9beb4.ttf"
+  },
+  {
+    "revision": "226ea55c3d9b36d4388f444c49b513a5",
+    "url": "/attendance-non-asn-webui/static/media/fa-brands-400.226ea55c.svg"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "/attendance-non-asn-webui/static/media/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "/attendance-non-asn-webui/static/media/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "/attendance-non-asn-webui/static/media/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "/attendance-non-asn-webui/static/media/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "/attendance-non-asn-webui/static/media/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "/attendance-non-asn-webui/static/media/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "6993975cbae77892abc356874c645730",
+    "url": "/attendance-non-asn-webui/static/media/fa-regular-400.6993975c.svg"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/attendance-non-asn-webui/static/media/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "/attendance-non-asn-webui/static/media/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "2a803d1830aa624f83dc0a79cfadb782",
+    "url": "/attendance-non-asn-webui/static/media/fa-solid-900.2a803d18.svg"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "/attendance-non-asn-webui/static/media/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "/attendance-non-asn-webui/static/media/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "/attendance-non-asn-webui/static/media/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "/attendance-non-asn-webui/static/media/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "9e1c415e6f3ef6580c06f5c39e70364e",
+    "url": "/attendance-non-asn-webui/static/media/logo.9e1c415e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2bcf970d17dcddca7a086dd2df5b57d9.js"
+  "/attendance-non-asn-webui/precache-manifest.6c249a0144b5b822ec4a78d1d5864edf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/attendance-non-asn-webui/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
